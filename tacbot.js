77e86d4d148451266f0886d130db1185cc2ff90e
Collin Myers <collin.myers@covenanteyes.com>
2017-06-16 16:56:59 -0400

    Tic tac toe is working...
    TODO: Input sanitization, cheat checking, etc…

--- tacbot.js
+++ tacbot.js
@@ -11,7 +11,7 @@ var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
-var token = process.env.SLACK_API_TOKEN || 'xoxb-198991807682-T7nzlYPduJpd0ufoXtbSTVqD'; //Your token here!
+var token = process.env.SLACK_API_TOKEN || ''; //Your token here!
 
 var rtm = new RtmClient(token, { 
   logLevel: 'debug',
@@ -23,6 +23,148 @@ var web = new WebClient(token);
 
 
 let channel;
+let gameboard = {};
+const possibleRoutes = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+var playFunc;
+
+
+// Tic Tac Toe logic
+function printBoard( user, channel ) {
+  let board = gameboard;
+  var boardString = "";
+      for( let y = 0; y < 3; ++y ) {
+        let row = ""
+        for( let x = 0; x < 3; ++x ) {
+          row += board[ y * 3 + x ] + "|";
+        }
+        boardString += row + "\n";
+      }
+      rtm.sendMessage(boardString, channel );
+}
+
+function hasWinrar( symbol, gameboard )
+{
+	for( route of possibleRoutes )
+  {
+  	if( isWinOnRoute( symbol, route, gameboard ) )
+    {
+    	return true;
+    }
+  }
+  return false;
+}
+
+function isWinOnRoute( symbol, indices, gameboard )
+{
+	return gameboard[indices[1]] == symbol && gameboard[indices[2]] == symbol && gameboard[indices[0]] == symbol;
+}
+
+//Collin
+function randomBot( symbol, gameboard )
+{
+	var openSpaces = [];
+	for( index in gameboard )
+  {
+  	if( gameboard[index] == '_')
+    {
+    	openSpaces.push(index);
+    }
+  }
+  var chosenSpace = openSpaces[Math.floor(Math.random() * openSpaces.length)];
+  
+  gameboard[chosenSpace] = symbol;
+}
+
+//Jeff
+function jeffWoffordBot( symbol, gameboard ) {
+
+    const otherSymbol = symbol == 'X' ? 'O' : 'X';
+
+    function findShallowWin() {
+      for( let i = 0; i < 9; ++i ) {
+        let board = [].concat( gameboard );
+      if( board[ i ] != '_' ) {
+          continue;
+      }
+      board[ i ] = symbol;
+      if( hasWinrar( symbol, board ))
+      {
+          return i;
+      }
+    }
+    return -1;
+  }
+  
+  function findShallowLoss() {
+      for( let i = 0; i < 9; ++i ) {
+        let board = [].concat( gameboard );
+      if( board[ i ] != '_' ) {
+          continue;
+      }
+      board[ i ] = otherSymbol;
+      if( hasWinrar( otherSymbol, board ))
+      {
+          return i;
+      }
+    }
+    return -1;
+  }
+
+    function calculateHalfTurnCount( gameboard ) {
+      let count = 0;
+    for( cell of gameboard ) {
+        if( cell != '_' ) {
+          ++count;
+      }
+    }
+    return count;
+  }
+
+    const halfTurnCount = calculateHalfTurnCount( gameboard )
+  const turnCount = Math.floor( halfTurnCount / 2 )
+    
+  // First move go center.
+  let cellToPlay = -1;
+  if( halfTurnCount == 0 ) {
+      cellToPlay = 4;
+  }
+  
+  // Other guy took center?
+  else if( halfTurnCount == 1 && gameboard[ 4 ] == otherSymbol ) {
+      // Take a corner.
+      cellToPlay = 0;
+  }
+  
+  else {
+      // Easy way to win?
+      cellToPlay = findShallowWin();
+    if( cellToPlay < 0 ) {
+    
+        // Easy way to prevent a loss?
+      cellToPlay = findShallowLoss();
+      
+      if( cellToPlay < 0 ) {    
+        // Just pick the first empty cell.
+        let options = [];
+        for( let i = 0; i < 9; ++i ) {
+          if( gameboard[ i ] == '_' ) {
+            options.push( i );
+          }
+        }
+        
+        cellToPlay = options[ Math.floor( Math.random() * options.length )];
+      }
+    }
+  }
+  
+  console.assert( cellToPlay >= 0 );
+  console.assert( gameboard[ cellToPlay ] == '_' );
+  gameboard[ cellToPlay ] = symbol;
+}
+
+
+
+// RTM callbacks
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   for (const c of rtmStartData.channels) {
@@ -35,23 +177,51 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   console.log('Message:', message);
   if(message.text.indexOf(rtm.activeUserId) > -1) //We've been tagged in a message.
   {
-    if(message.text.toLowerCase().indexOf("hello") > -1)
+    if(message.text.toLowerCase().indexOf("begin") > -1)
     {
-      rtm.sendMessage("Hello, " + rtm.dataStore.getUserById(message.user).real_name, message.channel);
-    }
-    if(message.text.toLowerCase().indexOf("info") > -1) // User requested info.  Give them the team name.
-    {
-      web.team.info(function teamInfoCallback(err, info) // Get team name using web API.
+      var code = message.text.toLowerCase().split(" ")[2];
+      var append = "";
+      if( code == "j" )
       {
-        if(err) 
+        playFunc = jeffWoffordBot;
+        append = "hard ";
+      }
+      else 
+      {
+        playFunc = randomBot;
+      }
+      rtm.sendMessage("Beginning new " + append + "game with " + rtm.dataStore.getUserById(message.user).real_name + "!", message.channel);
+      gameboard = ['_', '_', '_',
+								   '_', '_', '_',
+                   '_', '_', '_']; //Initialize the playing field.
+      
+
+      printBoard(gameboard, message.channel)
+      rtm.sendMessage("I go first!", message.channel);
+      playFunc('X', gameboard);
+      printBoard(gameboard, message.channel);
+      rtm.sendMessage("Your turn");
+
+    }
+    if(message.text.toLowerCase().indexOf("place") > -1)
+    {
+      console.log("Found place...*********************************************************************************************************************");
+      var place = message.text.split(" ")[2];
+      place = parseInt(place);
+      gameboard[place] = 'O'
+      if(hasWinrar('O', gameboard))
+      {
+        rtm.sendMessage("You win!", message.channel);
+      }
+      else
+      {
+        playFunc('X', gameboard);
+        if(hasWinrar('X', gameboard))
         {
-          console.log('Error getting team info from webAPI:', err);
+        rtm.sendMessage("collinbot wins!!", message.channel);
         }
-        else
-        {
-          rtm.sendMessage("Team info: " + info.team.name, message.channel);
-        }
-      });
+      }
+      printBoard(gameboard, message.channel);
     }
 
   }
@@ -59,6 +229,7 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
 
 rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
   console.log('Reaction added:', reaction);
+ 
 });
 
 rtm.on(RTM_EVENTS.TEAM_JOIN, function handleRtmTeamJoin(member) {
@@ -69,7 +240,6 @@ rtm.on(RTM_EVENTS.TEAM_JOIN, function handleRtmTeamJoin(member) {
 
 // you need to wait for the client to fully connect before you can send messages
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
-  rtm.sendMessage("Hello!", channel);
 });
 
 rtm.start();
